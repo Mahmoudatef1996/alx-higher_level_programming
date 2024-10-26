48b0233e59155c14b858897ceebf30033614cd29
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-10-26 09:43:55 -0700

    11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return a ^ b
+    """ Calculate the power of a number
+
+    Args:
+        a: the base number.
+        b: the exponent.
+    Returns: the result of a raised to the power of b.
+    """
+    if b == 0:
+        return 1
+    elif b > 0:
+        ans = 1
+        for _ in range(b):
+            ans *= a
+        return ans
+    else:
+        return 1 / pow(a, -b)
